6ed30f4a9176f08b56eb3551088f572babafbac1
sankareswarankaliraj <sankareswarankaliraj@gmail.com>
2023-08-04 22:03:30 +0530

    add form

--- src/App.js
+++ src/App.js
@@ -1,59 +1,90 @@
 import "./styles.css";
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
 export default function App() {
-  const[result,setResult]=useState("");
+  const [result, setResult] = useState("");
 
-  const handleclick=(e)=>
-  {
-    setResult(result.concat(e.target.name))
-  }
-  const clear=()=>
-  {
+  const handleclick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+  const clear = () => {
     setResult("");
-  }
-const backSpace=()=>{
-  setResult(result.slice(0, -1));
-}
-const calculate=()=>
-{
-  try{
-setResult(eval(result).toString())
-  }
-  catch(err){
-  setResult("evalution error")
-  }
-}
+  };
+  const backSpace = () => {
+    setResult(result.slice(0, -1));
+  };
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("evalution error");
+    }
+  };
   return (
     <div classname="container">
-      <br/>
-    <div classname="calculator">
-      <input type="text" placeholder="0" value={result}/>
-      <br/>
-      <button class="highlight" onClick={clear}>cl</button>
-      <button class="highlight" onClick={handleclick}>*</button>
-      <button class="highlight" name="/"onClick={handleclick}>/</button>
-      <button class="highlight"name="Bs"onClick={backSpace}>Bs</button>
-      <br/>
-      <button class="numeric" name="1" onClick={handleclick}>1</button>
-      <button name="2"onClick={handleclick}>2</button>
-      <button name="3"onClick={handleclick}>3</button>
-      <button class="highlight" name="-"onClick={handleclick}>-</button>
-      <br/>
-      <button name="4"onClick={handleclick}>4</button>
-      <button name="5"onClick={handleclick}>5</button>
-      <button name="6"onClick={handleclick}>6</button>
-      <button class="highlight" name="+"onClick={handleclick}>+</button>
-      
-      <br/>
-      <button name="7"onClick={handleclick}>7</button>
-      <button name="8"onClick={handleclick}>8</button>
-      <button name="9"onClick={handleclick}>9</button>
-      <button class="highlight" name="="onClick={calculate}>=</button>
-      <br/>
-      <button class="zero" name="0"onClick={handleclick}>0</button>
-      </div> 
-      </div> 
-     
-      );
+      <br />
+      <form>
+        <div classname="calculator">
+          <input type="text" placeholder="0" value={result} />
+          <br />
+          <button class="highlight" onClick={clear}>
+            cl
+          </button>
+          <button class="highlight" onClick={handleclick}>
+            *
+          </button>
+          <button class="highlight" name="/" onClick={handleclick}>
+            /
+          </button>
+          <button class="highlight" name="Bs" onClick={backSpace}>
+            Bs
+          </button>
+          <br />
+          <button class="numeric" name="1" onClick={handleclick}>
+            1
+          </button>
+          <button name="2" onClick={handleclick}>
+            2
+          </button>
+          <button name="3" onClick={handleclick}>
+            3
+          </button>
+          <button class="highlight" name="-" onClick={handleclick}>
+            -
+          </button>
+          <br />
+          <button name="4" onClick={handleclick}>
+            4
+          </button>
+          <button name="5" onClick={handleclick}>
+            5
+          </button>
+          <button name="6" onClick={handleclick}>
+            6
+          </button>
+          <button class="highlight" name="+" onClick={handleclick}>
+            +
+          </button>
+
+          <br />
+          <button name="7" onClick={handleclick}>
+            7
+          </button>
+          <button name="8" onClick={handleclick}>
+            8
+          </button>
+          <button name="9" onClick={handleclick}>
+            9
+          </button>
+          <button class="highlight" name="=" onClick={calculate}>
+            =
+          </button>
+          <br />
+          <button class="zero" name="0" onClick={handleclick}>
+            0
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
